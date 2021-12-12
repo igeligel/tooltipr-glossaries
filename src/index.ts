2d3d9540b823bab604e823d64ebe95a3efac7eb0
igeligel <kevinigeligeligel@gmail.com>
2021-12-12 16:48:35 +0100

    feat: glossaries
    add more glossaries

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
+import { awsGlossary } from "./glossaries/aws";
 import { financialIndependenceGlossary } from "./glossaries/financial-independence";
+import { frontendRoadmapGlossary } from "./glossaries/frontend-roadmap";
+import { techRecruitmentGlossary } from "./glossaries/tech-recruitment";
 import { Dictionary, Glossary, TermItem } from "./types";
 
 export { financialIndependenceGlossary };
+export { awsGlossary };
+export { frontendRoadmapGlossary };
+export { techRecruitmentGlossary };
 export { Dictionary, TermItem, Glossary };
